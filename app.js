125f1fa5cb20b7f5b2cb0fdf5920847e8f744ed3
Trungdp <trungtrs1998@gmail.com>
2019-12-20 14:29:37 +0700

    asd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var server = require('http').createServer(app);
 var fs = require('fs');
 var bodyParser = require('body-parser');
-const Route = require('./routes/index');
+const Route = require('./routes/Index');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
